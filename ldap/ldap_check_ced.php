1f8a701f0ca75f3e8653f9c392df3a678fdfd0af
Kevin Mora <kevin.morag94@gmail.com>
2017-05-29 20:36:18 -0600

    php coding standard en las conexiones ldap

--- ldap/ldap_check_ced.php
+++ ldap/ldap_check_ced.php
@@ -1,46 +1,63 @@
 <?php
-	// Obtiene los métodos de wordpress
-	require_once("../../../../wp-load.php");
+/** 
+Obtiene los métodos de wordpress
 
-	function check_ldap_cedula($ced) {
-		$ldapserver  = 'ldaps://ldap.mifirmacr.org';
-		$ldapbn      = "cn=admin,dc=ldap,dc=mifirmacr,dc=org";
-		$ldapbnpass  = "AX7coRbA8dP6UoujeRFLv99Wf3N";
-		$ldaptree    = "dc=ldap,dc=mifirmacr,dc=org";
+@category Firma_Digital
+@package  Mifirma
+@author   Michael Vinicio Rodríguez Delgado <mvrd_17@hotmail.com>
+@license  MIT <https://opensource.org/licenses/MIT>
+@link     nubila.tech
+*/
 
-		$ldapconn = ldap_connect($ldapserver) or die("Could not connect to LDAP server.");
+require_once "../../../../wp-load.php";
 
-		if($ldapconn){
+/**
+ * Revisa si existe la cédula con la que se quiere firmar en el directorio de 
+ * usuarios
+ *
+ * @param String $ced cédula del usuario
+ * 
+ * @return Array or Boolean
+ */
+function check_ldap_cedula($ced)
+{
+    $ldapserver  = 'ldaps://ldap.mifirmacr.org';
+    $ldapbn      = "cn=admin,dc=ldap,dc=mifirmacr,dc=org";
+    $ldapbnpass  = "AX7coRbA8dP6UoujeRFLv99Wf3N";
+    $ldaptree    = "dc=ldap,dc=mifirmacr,dc=org";
 
-			ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
-	    	$ldapbind = ldap_bind($ldapconn, $ldapbn, $ldapbnpass);
+    $ldapconn = ldap_connect($ldapserver) or die("Could not connect to LDAP server.");
 
-	    	if($ldapbind){
-	    		$result = ldap_search($ldapconn,$ldaptree,"(employeenumber=".$ced.")") or die ("Error in search query: ".ldap_error($ldapconn));
+    if ($ldapconn) {
+        ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
+        $ldapbind = ldap_bind($ldapconn, $ldapbn, $ldapbnpass);
 
-		        $data = ldap_get_entries($ldapconn, $result);
+        if ($ldapbind) {
+            $result = ldap_search($ldapconn, $ldaptree, "(employeenumber=".$ced.")") 
+            or die("Error in search query: ".ldap_error($ldapconn));
+            $data = ldap_get_entries($ldapconn, $result);
 
-		        if($data["count"] > 0){
-		        	// ced aceptada
-		        	$employee_role = $data[0]["employeetype"][0];
-		        	return ["exist" => true, "role" => $employee_role];
-		        }else{
-		        	// ced NO aceptada
-		        	return false;
-		        }
+            if ($data["count"] > 0) {
+                // ced aceptada
+                $employee_role = $data[0]["employeetype"][0];
+                return ["exist" => true, "role" => $employee_role];
+            } else {
+                // ced NO aceptada
+                return false;
+            }
 
-	    	}else{
-	    		//LDAP bind failed...
-		        header("Location: ../login_form.php?alert=credenciales con ldap incorrectos");
-		        die();
-	    	}
+        } else {
+            //LDAP bind failed...
+            header("Location: ../login_form.php?alert=credenciales con ldap incorrectos");
+            die();
+        }
 
-		}else{
-			// LDAP conection failed...
-		    header("Location: ../login_form.php?alert=conexión con ldap falló");
-		    die();
-		}
-		ldap_close($ldapconn);
-	}
+    } else {
+        // LDAP conection failed...
+        header("Location: ../login_form.php?alert=conexión con ldap falló");
+        die();
+    }
+        ldap_close($ldapconn);
+}
 
 ?>
--- ldap/ldap_login.php
+++ ldap/ldap_login.php
@@ -1,8 +1,8 @@
 <?php
 
 // Obtiene los métodos de wordpress
-require_once("../../../../wp-load.php");
-require_once("../auth/auth.php");
+require_once "../../../../wp-load.php";
+require_once "../auth/auth.php";
 
 // Obtiene los datos del usuario que ingresaron por el formulario
 $user = $_POST['user'];
@@ -28,19 +28,19 @@ $ldapbnpass  = "AX7coRbA8dP6UoujeRFLv99Wf3N";
 
 // connect 
 $ldapconn = ldap_connect($ldapserver) or die("Could not connect to LDAP server.");
-if($ldapconn) {
+if ($ldapconn) {
     // binding to ldap server
     ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
     ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
     $ldapbind = ldap_bind($ldapconn, $ldapbn, $ldapbnpass);
-    // or die ("Error trying to bind: ".ldap_error($ldapconn))
-    	
+    // or die ("Error trying to bind: ".ldap_error($ldapconn))    	
     // verify binding
     if ($ldapbind) {
         //echo "LDAP bind successful...<br /><br />";
         
         
-        $result = ldap_search($ldapconn,$ldaptree,$ldapuser) or die ("Error in search query: ".ldap_error($ldapconn));
+        $result = ldap_search($ldapconn, $ldaptree, $ldapuser) 
+        or die("Error in search query: ".ldap_error($ldapconn));
         
         $data = ldap_get_entries($ldapconn, $result);        
         
@@ -49,19 +49,17 @@ if($ldapconn) {
         // print_r($data);    
         //echo '</pre>';
 
-	        if($data["count"] > 0){
-	        	for ($i=0; $i<$data["count"]; $i++) {
-
-		            $user_uid       = $data[$i]["uid"][0];
-		            $user_ldap_pass = $data[$i]["userpassword"][0];
-		            $user_role      = $data[$i]["employeetype"][0];		            
-
-		        }
-		        //print_r($user_role);
-	        }else{
-	        	header("Location: ../login_form.php?alert=usuario incorrecto");
-        		die();
-	        }	                            
+        if ($data["count"] > 0) {
+            for ($i=0; $i<$data["count"]; $i++) {
+                $user_uid       = $data[$i]["uid"][0];
+                $user_ldap_pass = $data[$i]["userpassword"][0];
+                $user_role      = $data[$i]["employeetype"][0];
+            }
+            //print_r($user_role);
+        } else {
+            header("Location: ../login_form.php?alert=usuario incorrecto");
+            die();
+        }
         
         // var_dump($user_ced);
     } else {
@@ -73,11 +71,11 @@ if($ldapconn) {
 // all done? clean up
 ldap_close($ldapconn);
 
-if($user_ldap_pass != $ldappass){
-	header("Location: ../login_form.php?alert=contraseña incorrecta");
+if ($user_ldap_pass != $ldappass) {
+    header("Location: ../login_form.php?alert=contraseña incorrecta");
     die();
-}else{
-	create_local_user($user_uid, $user_password, $user_role);
+} else {
+    create_local_user($user_uid, $user_password, $user_role);
 }
 
 
